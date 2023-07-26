ba3b7b24c18f22e6c253ffc2e527f52c56178d6d
ubo-dev <umutokur11@outlook.com>
2023-07-26 15:29:29 +0300

    dependencies and docker compose are added

---
+++ spring-cloud-config-server/src/test/java/com/ubo/scmicroservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubo.scmicroservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
